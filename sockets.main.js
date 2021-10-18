5ff6fdb9e674e29400dfdb125d9074e77ae85dab
Daniel Hernandez <dhernandez30@ucol.mx>
2021-10-18 18:44:47 -0500

    preparando server

--- sockets.main.js
+++ sockets.main.js
@@ -4,11 +4,13 @@ const query = require('./database');
 //esta linea abre la coneccion y recibe el socket para conectarnos
 io.on("connection", (socket) => {
    //este socket esta a la escucha, cuando llegan mensajes los guarda en la bd y por ultimo los envia
-  socket.on("conectado", async (mensaje) => {
-    
+  socket.on("mensaje", async (mensaje) => {
+    console.log(mensaje);
     io.emit('conectado', mensaje)
   })
-
+  socket.on("conectado", (nombre) => {
+    io.emit("prueba", nombre)
+  })
   //Este socket escucha si un cliente se desconecta
   socket.on("disconnect", () => {
     io.emit('mensajes', {nombre: "Servidor", mensaje: "Ha abandonado la sala"})
