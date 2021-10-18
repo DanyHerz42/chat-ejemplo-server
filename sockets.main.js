f32c1bee0c044026e47779c158b1db2d7844127c
Daniel Hernandez <dhernandez30@ucol.mx>
2021-10-18 18:25:20 -0500

    preparando server

--- sockets.main.js
+++ sockets.main.js
@@ -4,7 +4,7 @@ const query = require('./database');
 //esta linea abre la coneccion y recibe el socket para conectarnos
 io.on("connection", (socket) => {
    //este socket esta a la escucha, cuando llegan mensajes los guarda en la bd y por ultimo los envia
-  socket.on("mensaje", async (mensaje) => {
+  socket.on("conectado", async (mensaje) => {
     
     io.emit('mensajes', mensaje)
   })
