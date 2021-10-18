ed0c5ae24b6d410c213ca83e561667719437aa9a
Daniel Hernandez <dhernandez30@ucol.mx>
2021-10-18 18:58:22 -0500

    preparando server

--- sockets.main.js
+++ sockets.main.js
@@ -9,7 +9,7 @@ io.on("connection", (socket) => {
     io.emit('conectado', mensaje)
   })
   socket.on("conectado", ({to, from, body}) => {
-    io.emit(to, body);
+    // io.emit(to, body);
     io.emit(from, body);
   })
   //Este socket escucha si un cliente se desconecta
